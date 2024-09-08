f11eea8586059556cc01cb0b435648f883d4643d
RenatoVA <renato.vivas2001@gmail.com>
2024-09-08 10:24:40 -0500

    First commit

---
+++ src/main/java/com/grupo1software/youngmiracles/YoungMiraclesApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1software.youngmiracles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungMiraclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungMiraclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo1software/youngmiracles/YoungMiraclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1software.youngmiracles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungMiraclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
