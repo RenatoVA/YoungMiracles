b6b21319ccc0157532c14870a1ca411fd8877221
RenatoVA <renato.vivas2001@gmail.com>
2024-09-25 22:14:26 -0500

    HU-15 completado

--- src/main/java/com/grupo1software/youngmiracles/dto/MensajeDTO.java
+++ src/main/java/com/grupo1software/youngmiracles/dto/MensajeDTO.java
@@ -3,6 +3,8 @@ package com.grupo1software.youngmiracles.dto;
 import com.grupo1software.youngmiracles.model.entity.Familiar;
 import com.grupo1software.youngmiracles.model.entity.Voluntario;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -11,8 +13,11 @@ import java.time.LocalDateTime;
 public class MensajeDTO {
 
     private Long id;
+    @NotBlank(message= "El mensaje es obligatorio")
     private String texto;
+    @NotBlank(message= "El voluntario es obligatorio")
     private Voluntario voluntario;
+    @NotBlank(message= "El familiar es obligatorio")
     private Familiar familiar;
     private LocalDateTime fechaEnvio;
 }
