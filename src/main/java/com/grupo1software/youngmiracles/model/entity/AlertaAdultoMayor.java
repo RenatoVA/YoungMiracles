38d69355a3a64c55723d356c2ff361ab57b8389c
HectorQR <u20201b280@upc.edu.pe>
2024-09-26 08:21:54 -0500

    Correción del codigo

--- src/main/java/com/grupo1software/youngmiracles/model/entity/AlertaAdultoMayor.java
+++ src/main/java/com/grupo1software/youngmiracles/model/entity/AlertaAdultoMayor.java
@@ -1,14 +1,18 @@
 package com.grupo1software.youngmiracles.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "alerta_adulto_mayor")
 public class AlertaAdultoMayor {
     @Id
