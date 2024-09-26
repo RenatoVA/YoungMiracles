f3d50a5515a807a1392c32c7202186eb32447be9
RenatoVA <renato.vivas2001@gmail.com>
2024-09-25 22:14:56 -0500

    HU-16 completado

--- src/main/java/com/grupo1software/youngmiracles/model/entity/Sesion.java
+++ src/main/java/com/grupo1software/youngmiracles/model/entity/Sesion.java
@@ -1,7 +1,5 @@
 package com.grupo1software.youngmiracles.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
@@ -10,15 +8,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "sesiones")
 @Inheritance(strategy = InheritanceType.JOINED)
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
-        property = "tiposesion"
-)
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = Fisioterapia.class, name = "fisioterapia"),
-        @JsonSubTypes.Type(value = Taller.class, name = "taller"),
-})
+
 public class Sesion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
